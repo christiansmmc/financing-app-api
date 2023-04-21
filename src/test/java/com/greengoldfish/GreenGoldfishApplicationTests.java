5d2a858d2ef2d5fcb08c97fb3d5ace05422366c3
Christian Sequeira <csequeira153@gmail.com>
2023-04-20 22:39:38 -0300

    Initial commit

---
+++ src/test/java/com/greengoldfish/GreenGoldfishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greengoldfish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenGoldfishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
